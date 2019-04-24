995aba977d6872ab97673e7ad3538da8075beaab
gunawan <gunawan@gmail.com>
2019-04-24 12:17:00 +0700

    second commit

---
+++ responsiproject2370 (1)/Program (1).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsiproject2370
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Karyawan kry1 = new Karyawan();
+
+            kry1.Nik = "18998811";
+            kry1.Nama = "yudha pratama";
+            kry1.GajiBulanan = 4000000;
+
+            Karyawan kry2 = new Karyawan();
+            kry2.Nik = "342656742";
+            kry2.Nama = "kurniady";
+            kry2.GajiBulanan = 3000000;
+
+            Console.WriteLine("No\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("____________________________________________________");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}\n", kry1.Nik, kry1.Nama, kry1.GajiBulanan);
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}\n", kry2.Nik, kry2.Nama, kry2.GajiBulanan);
+            
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Asyiikk Kenaikan Gaji 10%\n");
+            Console.WriteLine("No\tNik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("____________________________________________________");
+            Console.WriteLine("1.\t{0}  {1}\t\t{2}\n", kry1.Nik, kry1.Nama, kry1.GajiBulanan + (kry1.GajiBulanan * 0.10));
+            Console.WriteLine("2.\t{0}  {1}\t\t{2}\n", kry2.Nik, kry2.Nama, kry2.GajiBulanan + (kry2.GajiBulanan * 0.10));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ responsiproject2370 (1)/karyawan (1).cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsiproject2370
+{
+    class Karyawan
+    {
+
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        
+        public Karyawan(string nik, string nama, int gajibulanan)
+        {
+
+
+            if (GajiBulanan < 0)
+            {
+                gajibulanan = 0;
+                Nik = nik;
+                Nama = nama;
+                GajiBulanan = gajibulanan;
+
+            }
+            
+
+        }
+
+        public Karyawan()
+        {
+        }
+    }
+}
+
